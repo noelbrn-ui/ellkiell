caaf221f3274d2a2c9965a289ef42a43d6c98851
ffndraa <haffindra@gmail.com>
2026-04-24 10:23:10 +0700

    elll

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_pemburu_zombie": {
+        judul: "Sistem Pemburu Zombie untuk masa depan",
+        keahlian: ["Bantai musuh pakai Desert Eagle" , "Schyte"],
+        gambar: "https://egw.news/_next/image?url=https%3A%2F%2Fegw.news%2Fuploads%2Fnews%2F1%2F17%2F1761830982706_1761830982706.webp&w=1920&q=75",
+        latar: "Noel Scott Kennedy adalah agen federal yang bekerja untuk D.S.O. (Division of Security Operations). Noel digambarkan memiliki keturunan Italia-Amerika. Dalam beberapa entri terbaru, karakternya terus berkembang sebagai sosok agen veteran yang berdedikasi tinggi dalam melawan ancaman bio-terorisme global",
+        tantangan: "menghadapi gelombang serangan zombie yang semakin meningkat, Noel harus mengembangkan strategi baru untuk melindungi manusia dan menemukan cara untuk menghentikan penyebaran virus zombie yang mematikan.",
+        pelajaran: "Dalam menghadapi krisis zombie, Noel belajar untuk bekerja sama dengan berbagai pihak, termasuk ilmuwan, militer, dan masyarakat umum. Dia menyadari pentingnya kolaborasi dan komunikasi dalam mengatasi ancaman global yang kompleks.",
+        link: ""
+    },
+};
